2f00dd6a30a5e3336c39ecf491848a02a0e80302
motvr <yousefaweda1@gmail.com>
2024-06-08 14:39:45 +0300

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -26,12 +26,12 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
+            InlineKeyboardButton(text=_["S_B_10"], url=f"https://t.me/D_Z_J_C"),
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
+            InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/Jaithon"),
+            InlineKeyboardButton(text=_["S_B_2"], url=f"https://t.me/Source_MRVR")
         ],
     ]
     return buttons
