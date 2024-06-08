515a71b4f5e43ecc5a762536e70c260658b9a73e
motvr <yousefaweda1@gmail.com>
2024-06-08 15:09:43 +0300

    Update مين في الكول.py

--- AnonXMusic/plugins/play/مين في الكول.py
+++ AnonXMusic/plugins/play/مين في الكول.py
@@ -13,7 +13,7 @@ from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError,AlreadyJ
 async def strcall(client, message):
     assistant = await group_assistant(Anony,message.chat.id)
     try:
-        await assistant.join_group_call(message.chat.id, AudioPiped("./AnonXMusic/assets/call.mp3"), stream_type=StreamType().pulse_stream)
+        await assistant.join_group_call(message.chat.id, AudioPiped("https://graph.org/file/217aac5f9cd2b05f7ba5a.mp4"), stream_type=StreamType().pulse_stream)
         text="🔔 الاعضاء المتواجدين في الكول :\n\n"
         participants = await assistant.get_participants(message.chat.id)
         k =0
