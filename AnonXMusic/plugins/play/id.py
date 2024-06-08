d49fdd3ea4ecf9ddf2135d18f5bc424efb69445e
motvr <yousefaweda1@gmail.com>
2024-06-08 15:58:15 +0300

    Update id.py

--- AnonXMusic/plugins/play/id.py
+++ AnonXMusic/plugins/play/id.py
@@ -41,31 +41,6 @@ async def iddopen(client, message):
       return await message.reply_text("لازم تكون ادمن يشخه علشان اسمع كلامك")
 
 
-
-
-@app.on_message(
-    filters.command(["ايدي","id","ا"], "")
-& filters.group
-)
-async def iddd(client, message):
-    if message.chat.id in iddof:
-      return
-    usr = await client.get_chat(message.from_user.id)
-    name = usr.first_name
-    photo = await app.download_media(usr.photo.big_file_id)
-    await message.reply_photo(photo,       caption=f"""🤡 ¦𝙽𝙰𝙼𝙴 :{message.from_user.mention}\n🎯 ¦𝚄𝚂𝙴𝚁 :@{message.from_user.username}\n🎃 ¦𝙸𝙳 :`{message.from_user.id}`\n💌 ¦𝙱𝙸𝙾 :{usr.bio}\n✨ ¦𝙲𝙷𝙰𝚃: {message.chat.title}\n♻️ ¦𝙸𝙳.𝙶𝚁𝙾𝚄𝙿 :`{message.chat.id}`""", 
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        ),
-    )
-
-
-
 iddof = []
 @app.on_message(
     filters.command(["قفل جمالي","تعطيل جمالي"], "")
