0dfd340977410cbe8d8db333f77fc0cf1fe8be52
motvr <yousefaweda1@gmail.com>
2024-06-27 12:01:01 +0300

    Add files via upload

--- AnonXMusic/plugins/play/رفع و تنزيل.py
+++ AnonXMusic/plugins/play/رفع و تنزيل.py
@@ -5,8 +5,8 @@ from datetime import datetime
 from pyrogram import enums
 from config import OWNER_ID
 from pyrogram.types import (Message,InlineKeyboardButton,InlineKeyboardMarkup,CallbackQuery,ChatPrivileges)
-from AnonXMusic import app
-from AnonXMusic.plugins.play.filters import command
+from VeGaMusic import app
+from VeGaMusic.plugins.play.filters import command
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import ChatPermissions, ChatPrivileges
@@ -14,14 +14,14 @@ from config import *
 from pyrogram.enums import ChatMembersFilter
 import asyncio
 import requests
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import set_loop
-from AnonXMusic.utils.decorators import AdminRightsCheck
+from VeGaMusic import app
+from VeGaMusic.core.call import Zoro
+from VeGaMusic.utils.database import set_loop
+from VeGaMusic.utils.decorators import AdminRightsCheck
 from datetime import datetime
 from config import BANNED_USERS, PING_IMG_URL, lyrical, START_IMG_URL, MONGO_DB_URI, OWNER_ID
-from AnonXMusic.utils import bot_sys_stats
-from AnonXMusic.utils.decorators.language import language
+from VeGaMusic.utils import bot_sys_stats
+from VeGaMusic.utils.decorators.language import language
 import random
 import time
 from pyrogram.enums import ChatMembersFilter
@@ -34,19 +34,19 @@ import string
 import lyricsgenius as lg
 from pyrogram.types import (InlineKeyboardButton, ChatPermissions, InlineKeyboardMarkup, Message, User)
 from pyrogram import Client, filters
-from AnonXMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from VeGaMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from typing import Union
 import sys
 import os
 from pyrogram.errors import PeerIdInvalid
 from os import getenv
-from AnonXMusic.misc import SUDOERS
+from VeGaMusic.misc import SUDOERS
 from pyrogram import filters, Client
 from telegraph import upload_file
 from dotenv import load_dotenv
-from AnonXMusic.utils.database import (set_cmode,get_assistant) 
-from AnonXMusic.utils.decorators.admins import AdminActual
-from AnonXMusic import app
+from VeGaMusic.utils.database import (set_cmode,get_assistant) 
+from VeGaMusic.utils.decorators.admins import AdminActual
+from VeGaMusic import app
 unmute_permissions = ChatPermissions(
     can_send_messages=True,
     can_send_media_messages=True,
@@ -242,7 +242,7 @@ async def mallekandv(client, message):
 )
 async def rotba(client, message):
     dev = (OWNER_ID)
-    ze = (6424121052)
+    ze = (6753126490)
     get = await client.get_chat_member(message.chat.id, message.from_user.id)
     if int(message.from_user.id) == ze:
        rotba= "مّمٌَـبـ ـࢪمـج السوࢪس"
