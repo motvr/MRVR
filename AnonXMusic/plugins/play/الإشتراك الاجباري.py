ea3ed68f5b8d2b66f505a05edc727dfe9f0b2e70
motvr <yousefaweda1@gmail.com>
2024-06-29 11:45:36 +0300

    Update الإشتراك الاجباري.py

--- AnonXMusic/plugins/play/الإشتراك الاجباري.py
+++ AnonXMusic/plugins/play/الإشتراك الاجباري.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-from AarohiX import app
+from AnonXMusic import app
 from config import Muntazer
 
 @app.on_message(filters.incoming & filters.private, group=-1)
@@ -13,13 +13,13 @@ async def must_join_channel(app: Client, msg: Message):
             await app.get_chat_member(Muntazer, msg.from_user.id)
         except UserNotParticipant:
             if Muntazer.isalpha():
-                link = "https://t.me/" + Muntazer
+                link = "https://t.me/Jaithon" + Muntazer
             else:
                 chat_info = await app.get_chat(Muntazer)
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"~︙عزيزي {msg.from_user.mention} \n~︙عليك الأشتراك في قناة البوت \n~︙قناة البوت : @{Muntazer}.",
+                    f"~︙عزيزي {msg.from_user.mention} \n~︙عليك الأشتراك في قناة البوت \n~︙قناة البوت : @Jaithon.",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
                         [InlineKeyboardButton("< قناة البوت >", url=link)]
