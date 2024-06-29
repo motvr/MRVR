78059e748e577bfb66cc173be331452e2e89afa7
motvr <yousefaweda1@gmail.com>
2024-06-29 11:50:09 +0300

    Update الإشتراك الاجباري.py

--- AnonXMusic/plugins/play/الإشتراك الاجباري.py
+++ AnonXMusic/plugins/play/الإشتراك الاجباري.py
@@ -1,8 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-from AnonXMusic import app
-from config import Jaithon 
+from AnonXMusic import app 
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
